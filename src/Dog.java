359a04be6103a4f2fe8f43898fbd21b82bfca941
Thando Mini <tzone85@gmail.com>
2017-01-23 13:23:06 +0200

    initial commit with the basic structures of the animal class

---
+++ src/Dog.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Thando Mini on 1/23/2017.
+ */
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int brain, int body, int size, int weight) {
+        super(name, brain, body, size, weight);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Thando Mini on 1/23/2017.
+ */
+public class Main {
+    public static void main(String [] args){
+
+    }
+}
